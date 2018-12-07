4ca5a854025bd7879a78bf12a40c11c393a29664
gomao <gomaoq@gmail.com>
2018-12-08 00:43:22 +0900

    fix link

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 
 require 'csv'
 require 'open-uri'
-records = CSV.table(open('/Users/gomao/Downloads/tb.csv'))
+records = CSV.table(open('https://dl.dropboxusercontent.com/s/7cl0209tlwggbra/tb.csv'))
 max = Score.maximum(:datetime)
 records = records.select { |r| Time.parse(r[:datetime]) > max } if max
 records.each do |r|
