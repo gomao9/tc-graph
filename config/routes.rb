36517e97da6cf84a458f269b262fb6c246e7cbf9
gomao <gomaoq@gmail.com>
2018-12-07 23:41:40 +0900

    fix navbar link

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "scores#index"
+  get ':subject', to: 'scores#index'
   resources :scores do
     get :wastes, on: :collection
     get :changes, on: :collection
