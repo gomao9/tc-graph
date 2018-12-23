8a5173123e297941d5ec792358137d5f52f0fe41
gomao <gomaoq@gmail.com>
2018-12-23 13:56:50 +0900

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "scores#index"
+  get 'about', to: 'about#index'
   get 'wastes', to: 'scores#wastes'
   get 'changes', to: 'scores#changes'
   get ':subject', to: 'scores#index'
