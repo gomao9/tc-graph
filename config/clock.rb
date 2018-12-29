7128cb6b0dce94e6051597a7f84fffbffcfde643
gomao <gomaoq@gmail.com>
2018-12-29 11:56:13 +0900

    add task

--- config/clock.rb
+++ config/clock.rb
@@ -3,6 +3,7 @@ require File.expand_path('../environment', __FILE__)
 require 'clockwork'
 require 'open-uri'
 
+SUBJECTS = %w(主人公 友達 先生 館の女主人 メイド 少女 妖精 魔法使い オオカミ 旅人 ダスク バスターブレイド アマリリス ベルベット ファイナルデイ)
 
 module Clockwork
   every(1.minute, 'minutely update') do
@@ -31,4 +32,13 @@ module Clockwork
       end
     end
   end
+
+  every(5.minute, 'graph cache') do
+     Rails.cache.write("dashboard", Score.dashboard_graphs)
+     SUBJECTS.each do |subject|
+       Rails.cache.write("index/#{subject}/graph", Score.detail_graph(subject))
+     end
+  end
+
 end
+
