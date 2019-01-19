795a1c9a5dff7b43e79fd4d2d0a883eb3ad1bcf1
gomao <gomaoq@gmail.com>
2019-01-20 01:28:56 +0900

    remove insert

--- config/clock.rb
+++ config/clock.rb
@@ -6,34 +6,7 @@ require 'open-uri'
 SUBJECTS = %w(主人公 友達 先生 館の女主人 メイド 少女 妖精 魔法使い オオカミ 旅人 ダスク バスターブレイド アマリリス ベルベット ファイナルデイ)
 
 module Clockwork
-  every(1.minute, 'minutely update') do
-    casts = JSON.parse open('https://api.matsurihi.me/mltd/v1/election/current?prettyPrint=false').read
-
-    datetime = nil
-    scores = casts.flat_map do |cast|
-      subject = cast['name']
-      datetime = DateTime.parse(cast['summaryTime'])
-      cast['data'].first.map do |idol_rank|
-        idol_rank['idol'] = idol_rank.delete('idol_name')
-        idol_rank['datetime'] = datetime
-        idol_rank['subject'] = subject
-
-        idol_rank.delete('idol_id')
-
-        idol_rank
-      end
-    end
-
-    unless Score.exists?(datetime: datetime)
-      Score.transaction do
-        scores.each do |s|
-          Score.find_or_create_by(s)
-        end
-      end
-    end
-  end
-
-  every(5.minute, 'graph cache') do
+  every(1.hour, 'graph cache') do
      Rails.cache.write("dashboard", Score.dashboard_graphs)
      SUBJECTS.each do |subject|
        Rails.cache.write("index/#{subject}/graph", Score.detail_graph(subject))
